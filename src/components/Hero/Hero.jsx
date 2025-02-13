f642f4e8e77793227d671069e61777efe0d76d4f
Heitor <heitor.moreira.neto@outlook.com>
2025-02-13 12:33:17 -0300

    .....

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -20,7 +20,7 @@ Estou em busca de uma oportunidade para aplicar meu conhecimento, crescer profis
         </a>
       </div>
       <img
-        src={"../assets/test.png"}
+        src={"../assets/download.jpeg"}
         alt="Hero image of me"
         className={styles.heroImg}
       />
