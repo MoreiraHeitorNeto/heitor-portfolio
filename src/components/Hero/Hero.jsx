dd0a740725f29d63ab604ea065ae912fdd6e6b7e
Heitor <heitor.moreira.neto@outlook.com>
2025-02-13 13:05:27 -0300

    .

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
-  const newLocal = "https://scontent.ftow5-1.fna.fbcdn.net/v/t39.30808-6/344853999_536162861822742_483870715512451778_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=hvbcH6Bvf1oQ7kNvgGy1N-d&_nc_oc=Adg3AZzFIhm6fPjYgzooFDOd6jOOXcJf9vuxeYpIrSzAF-GhvWQPG2TJ11kTkaKFX0E&_nc_zt=23&_nc_ht=scontent.ftow5-1.fna&_nc_gid=A2JXPzEKhyiabGTCBee69Be&oh=00_AYBeh3lkcynVq9QLWscZOYSID1TNZjEuIH3hRgLUnWCySQ&oe=67B3D083";
+  const newLocal = "../assets/peloamor.jpg";
   return (
     <section className={styles.container}>
       <div className={styles.content}>
