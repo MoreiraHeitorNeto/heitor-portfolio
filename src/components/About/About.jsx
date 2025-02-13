79ba5191c33eaaf69b03eac7545bdbf03467c1cc
Heitor <heitor.moreira.neto@outlook.com>
2025-02-13 13:23:58 -0300

    ajustando imgs da section about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,13 +9,13 @@ export const About = () => {
       <h2 className={styles.title}>Sobre</h2>
       <div className={styles.content}>
         <img
-          src={getImageUrl("about/dev.png")}
+          src="/dev.png"
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <img src="/cursorIcon.png" alt="Cursor icon" />
             <div className={styles.aboutItemText}>
               <h3>Frontend Developer</h3>
               <p>
@@ -25,7 +25,7 @@ export const About = () => {
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <img src="/cursorIcon.png" alt="UI icon" />
             <div className={styles.aboutItemText}>
               <h3>UI Designer</h3>
               <p>
