87ee42ef291a81814cdcbcf19adb8830252904d2
Heitor <heitor.moreira.neto@outlook.com>
2025-02-13 12:54:09 -0300

    ....

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
-  const newLocal = "../assets/heroImageMan.png";
+  const newLocal = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMwAAADACAMAAAB/Pny7AAABDlBMVEX+AAD/AP8A/wH//wEB//8AAAAAAGz///8AAP7///z//56P/6MQ//UB/9la4FnjVeMYAPb/ANeJAKIAAGdtbX76+9Xl5fIkJG0AADnTWlpQ09MAmprU1FpRUdNa1FrTUdNQ01AAmgDUWtTTUFCaALucnP/19f+aAEEAACzKAHr+ADv/AOqbsJoi+SIA/0cB/+bY/2iamgBaWtP/9fX/nJwAALpRUVEAAEHU1NSaAJrU1IPU1Eya1JNh1Mpa1Lpa1ENm0WW+fb3UWr3TWkGVWpRQUDxRUcO5UHfYUEbTUL7aQ9qIs4fC02wAAExDQ8SZmenjnL7yi5OaAFGbAK9da10AmisAmouDmj5paYTw8NaUc0ZeAAABjElEQVR4nO3T11IUURRA0SYOWWVIShIQAxkM5KAEUUCEUcL//whVMFRNFw2neeBlWPsDbt91z+mkcFNzS12OWtvqg9pr4zpe1ES9SnLV+bKpsgQGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYG5hlhClWBWSq3sjoQt7b+Jmhjcyhs6/to1I/t13naWUiVfCu3uzcY93N/LOrXcNTvg7dhQxvRq113uJoqaSj3MT2x7D6Fg5+YDFeoZmo63MSZ2Wifr5tL/wUwMDAwMDAwMDAwMM8Ls1ju6GtFf47HM/v7pbKTD3c7Lb27t9LyTf9Gwv6f9efpfD5VUrytsaKLy/eZfU7V01u8W+P99TU8cUnWV4td3YUc9fQ+cPOMU6sJU1WTgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgYGBgXl0V4xopevcC4+dAAAAAElFTkSuQmCC";
   return (
     <section className={styles.container}>
       <div className={styles.content}>
